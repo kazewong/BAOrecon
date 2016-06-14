ff25e4532399bd55359dc94f480c87858fd5ed13
kazewong <wangkeiw@coma.hpc1.cs.cmu.edu>
2016-06-13 22:36:29 -0400

    RE-initial commit

--- readfits.py
+++ readfits.py
@@ -1,20 +1,44 @@
-import numpy as np
 from astropy.io import fits
-from reconFunction import*
 from scipy.integrate import quad
-
+import numpy as np
+import argparse
 def convert(z):
-	return 299792.458/((np.sqrt(0.3089*(1+np.power(z,3))+0.6911)*67.74)*0.6774)
+	return 2997.9245/(np.sqrt(0.3089*np.power(1+z,3)+0.6911))
 
-directory = '../data/'
-cata = fits.open(directory+'galaxy_DR10v8_LOWZ_North.fits')
+def coordinateTransform(z,RA,Dec):
+	output = np.array([[0. for i in range(z.size)] for i in range(3)])
+	output[0] = z*np.sin(Dec)*np.cos(RA)
+	output[1] = z*np.sin(Dec)*np.sin(RA)
+	output[2] = z*np.cos(Dec)
+	return output
+
+parser = argparse.ArgumentParser(description='Reconstructing galaxy catalog')
+parser.add_argument('-data',help='Data file directory')
+parser.add_argument('-output',default='./',help='The output file directory')
+args=parser.parse_args()
+
+file = np.genfromtxt(args.data).T
+RA,Dec,Z = np.radians(file[0]),np.radians(90-file[1]),file[2]
+for i in range(Z.size):
+	Z[i]=quad(convert,0,Z[i])[0]
+Cart = coordinateTransform(Z,RA,Dec)
+readout = np.array([Cart[0],Cart[1],Cart[2],[1. for i in range(Cart[0].size)]]).T
+np.savetxt(args.output,readout)
+
+'''
+directory = '/physics/wangkeiw/'
+cata = fits.open(directory+'random2_DR10v8_CMASS_North.fits')
 table = cata[1].data
-RA = table.field('RA')
-DEC = table.field('DEC')
+print table.field('RA')[-1],table.field('DEC')[-1]
+RA = np.radians(table.field('RA'))
+Dec = np.radians(90-table.field('DEC'))
 Z = table.field('Z')
-Density = table.field('nz')
+RA = RA[(Z>0)*(Z<2)]
+Dec = Dec[(Z>0)*(Z<2)]
+Z = Z[(Z>0)*(Z<2)]
+Density = table.field('nz')[(Z>0)*(Z<2)]
 for i in range(Z.size):
 	Z[i] = quad(convert,0,Z[i])[0]
-Cart = coordinateConvert(RA,DEC,Z)
-readout = np.array([Cart[0],Cart[1],Cart[2],Density]).transpose()
-np.savetxt(directory+'lowzNdata.dat',readout)
+Cart = coordinateTransform(Z,RA,Dec)
+readout = np.array([Cart[0],Cart[1],Cart[2],[1. for i in range(Cart[0].size)]]).transpose()
+np.savetxt(directory+'CMASSNrandom2.dat',readout)'''
--- reconFunction.py
+++ reconFunction.py
@@ -1,80 +1,148 @@
 import numpy as np
 from scipy import interpolate
 from scipy.spatial import distance
-def computeDensityField(position,density,gridNumber):
+def computeDensityField(position,density,gridNumber,boxsize,boxmin):
 	axis = np.array([0. for i in range(gridNumber)])
 	field = np.array(np.meshgrid(axis,axis,axis)[0])
 	position = position.T
 	index = position.copy()
-	index[0] = np.floor((position[0]-position[0].min())*(gridNumber-1)/(2242-position[0].min()))
-	index[1] = np.floor((position[1]-position[1].min())*(gridNumber-1)/(1425-position[1].min()))
-	index[2] = np.floor((position[2]-position[2].min())*(gridNumber-1)/(1128-position[2].min()))
+	index[0] = np.floor((position[0]-boxmin)*(gridNumber-1)/boxsize)
+	index[1] = np.floor((position[1]-boxmin)*(gridNumber-1)/boxsize)
+	index[2] = np.floor((position[2]-boxmin)*(gridNumber-1)/boxsize)
 	position = position.T
 	index = index.T
-	print density.size,index
 	for i in range(density.size):
-		if ((index[i][0]>0 and index[i][0]<gridNumber) and (index[i][1]>0 and index[i][1]<gridNumber) and (index[i][2]>0 and index[i][2]<gridNumber)):
+		if ((index[i][0]>=0 and index[i][0]<gridNumber) and (index[i][1]>=0 and index[i][1]<gridNumber) and (index[i][2]>=0 and index[i][2]<gridNumber)):
 			field[index[i][0]][index[i][1]][index[i][2]] =field[index[i][0]][index[i][1]][index[i][2]] + density[i]
 	return field
-
-def getWavenumber(size,boxsize):
-	axis = np.array([0. for i in range(size)])
-	wavenumber = np.array(np.meshgrid(axis,axis,axis)[0])
-	dk = 2*np.pi/boxsize
-	Axis = np.arange(1,size+1,dtype =float)
-	mesh = np.array(np.meshgrid(Axis,Axis,Axis))
-	xindex = mesh[1]*mesh[1]
-	yindex = mesh[0]*mesh[0]
-	zindex = mesh[2]*mesh[2]
-	wavenumber =dk*dk*(xindex+yindex+zindex)
-	return wavenumber
 
 def gaussianFilter(densityField,scale,wavenumber):
 	GFilter = wavenumber*scale*scale/2
 	GFilter = np.exp(-GFilter)
-	print densityField[-1][-1][-1],GFilter[-1][-1][-1],wavenumber[-1][-1][-1]
 	return densityField *GFilter
 
-def computeDisplacement(densityField,wavenumber,boxsize):
-	vectorAxis = np.array([[0.j,0.j,0.j] for i in range(densityField.shape[0])])
-	displacementField = np.array([[vectorAxis for i in range(densityField.shape[0])]for i in range(densityField.shape[0])])
+def getWavenumber(gridNumber,boxsize):
+	axis = np.array([0. for i in range(gridNumber)])
+	wavenumber = np.array(np.meshgrid(axis,axis,axis)[0])
 	dk = 2*np.pi/boxsize
-	Axis = np.arange(1,densityField.shape[0]+1)
+	Axis = np.arange(gridNumber,dtype =float)
 	mesh = np.array(np.meshgrid(Axis,Axis,Axis))
-	xindex = mesh[1].transpose()
-	yindex = mesh[0].transpose()
-	zindex = mesh[2].transpose()
-	displacementField = displacementField.transpose()
-	displacementField[0] = densityField.transpose()*-1j*dk*xindex/wavenumber.transpose()
-	displacementField[1] = densityField.transpose()*-1j*dk*yindex/wavenumber.transpose()
-	displacementField[2] = densityField.transpose()*-1j*dk*zindex/wavenumber.transpose()
-	displacementField = displacementField.transpose()
-	return displacementField
-
+	xindex = mesh[0]*mesh[0]
+	yindex = mesh[1]*mesh[1]
+	zindex = mesh[2]*mesh[2]
+	wavenumber =dk*dk*(xindex+yindex+zindex)
+	wavenumber[wavenumber==0]=1
+	return wavenumber
+	
 def coordinateConvert(RA,DEC,z):
 	Cart = np.array([[0. for i in range(RA.size)] for i in range(3)]) 
 	Cart[0] = z*np.cos(np.radians(RA))*np.cos(np.radians(DEC)) 
 	Cart[1] = z*np.sin(np.radians(RA))*np.cos(np.radians(DEC)) 
 	Cart[2] = z*np.sin(np.radians(DEC)) 
 	return Cart
-	
 
-def shiftParticle(original,displacement,gridNumber):
-	boxRange = np.ogrid[original.min():original.max():gridNumber*1j]
+def shiftParticle(original,displacement,gridNumber,boxsize,boxmin):
+	boxRange = np.arange(gridNumber)
 	boxGrid = np.array([boxRange for i in range(3)])
 	new = original.copy()
-	xinterp = interpolate.interpn(boxGrid,np.fft.ifftn(displacement[0]),new).real
-	yinterp = interpolate.interpn(boxGrid,np.fft.ifftn(displacement[1]),new).real
-	zinterp = interpolate.interpn(boxGrid,np.fft.ifftn(displacement[2]),new).real
-	new = new.transpose()
-	new[0] = new[0] +xinterp
-	new[1] = new[1] +yinterp
-	new[2] = new[2] +zinterp
+	new[0] = (original[0]-boxmin)*(gridNumber-1)/boxsize
+	new[1] = (original[1]-boxmin)*(gridNumber-1)/boxsize
+	new[2] = (original[2]-boxmin)*(gridNumber-1)/boxsize
+	xinterp = interpolate.interpn(boxGrid,displacement[0],new.T,bounds_error=False,fill_value = 0).real
+	yinterp = interpolate.interpn(boxGrid,displacement[1],new.T,bounds_error=False,fill_value = 0).real
+	zinterp = interpolate.interpn(boxGrid,displacement[2],new.T,bounds_error=False,fill_value = 0).real
+	print 'The first 10 interpolated value of x displacement is'
+	print new[0].min(),new[0].max(),new[1].min(),new[1].max(),new[2].min(),new[2].max(),xinterp.max(),yinterp.max(),zinterp.max(),xinterp.min(),yinterp.min(),zinterp.min()
+	new[0] = (new[0])*(boxsize)/(gridNumber-1)+boxmin+xinterp
+	new[1] = (new[1])*(boxsize)/(gridNumber-1)+boxmin+yinterp
+	new[2] = (new[2])*(boxsize)/(gridNumber-1)+boxmin+zinterp
 	new = new.transpose()
 	return new
 
+def maskDensityField(densityField,randomField):
+	output = densityField.copy()
+	mean = densityField[np.where(randomField!=0)].mean()
+	#mask = generatingMask(randomField,10,densityField.shape[0])
+	output[np.where(randomField==0)] = mean
+	output = output/mean -1	
+	return output
+	
+def getOffset(array,axis,distance):
+	output = array.copy()
+	if distance > 0:
+		for i in range(distance):
+			output = np.delete(output,0,axis)
+		for i in range(distance):
+			output = np.insert(output,output.shape[axis],0,axis)
+	if distance < 0:
+		for i in range(abs(distance)):
+			output = np.delete(output,output.shape[axis]-1,axis)
+		for i in range(abs(distance)):
+			output = np.insert(output,0,0,axis)
+	return output
 
+def getCenterIndex(position,gridNumber,boxsize):
+	position = position.T
+	x = np.floor(0.-position[0].min()*(gridNumber-1)/boxsize)
+	y = np.floor(0.-position[1].min()*(gridNumber-1)/boxsize)
+	z = np.floor(0.-position[2].min()*(gridNumber-1)/boxsize)
+	position = position.T
+	return np.array([x,y,z])
 
+def getDistance(gridNumber,boxsize,center):
+	gridLength = boxsize/gridNumber
+	Axis = np.arange(gridNumber)
+	mesh = np.meshgrid(Axis,Axis,Axis)
+	x = mesh[1]
+	y = mesh[0]
+	z = mesh[2]
+	x = gridLength*(x-center[0])
+	y = gridLength*(y-center[1])
+	z = gridLength*(z-center[2])
+	return np.array([x,y,z])
+	
+def getBoxSize(position):
+	boxsize = 0.
+	position = position.T
+	for i in range(3):
+		boxsize = boxsize + position[i].max()-position[i].min()
+	position = position.T
+	return boxsize/3
+
+def getRadialFactor(gridNumber,boxsize,boxmin):
+	axis=np.arange(float(gridNumber))
+	mesh=np.array(np.meshgrid(axis,axis,axis))
+	mesh-=np.floor(-boxmin*gridNumber/boxsize)
+	output=mesh.copy()
+	length=np.linalg.linalg.norm(mesh,axis=0)
+	output[0]=mesh[0]/length
+	output[1]=mesh[1]/length
+	output[2]=mesh[2]/length
+	output[np.where(np.isnan(output))]=0
+	return output
+	
+	
+def displacementSolver(dataField,randomField,gridSize):#The Jacobi method is used
+	h= 1./gridSize
+	h2 = 1./(gridSize**2)
+	axis = np.array([0. for i in range(dataField.shape[0]+2)])
+	potential = np.array(np.meshgrid(axis,axis,axis)[0])
+	for i in range(100):
+		potential[np.where(randomField==0)]=0
+		for j in range(3):
+			potential[1:gridSize+1,1:gridSize+1,1:gridSize+1] =  h2*dataField +getOffset(potential,j,1)[1:gridSize+1,1:gridSize+1,1:gridSize+1]+getOffset(potential,j,-1)[1:gridSize+1,1:gridSize+1,1:gridSize+1]
+		potential = potential/6
+	xdisplacement = (getOffset(potential[1:gridSize+1,1:gridSize+1,1:gridSize+1],0,1)-getOffset(potential[1:gridSize+1,1:gridSize+1,1:gridSize+1],0,-1))/(2*h)
+	ydisplacement = (getOffset(potential[1:gridSize+1,1:gridSize+1,1:gridSize+1],1,1)-getOffset(potential[1:gridSize+1,1:gridSize+1,1:gridSize+1],1,-1))/(2*h)
+	zdisplacement = (getOffset(potential[1:gridSize+1,1:gridSize+1,1:gridSize+1],2,1)-getOffset(potential[1:gridSize+1,1:gridSize+1,1:gridSize+1],2,-1))/(2*h)
+	print "the min-max potential is:"+str(potential.min())+str(potential.max())
+        print "The min-max x displacement is:"+str(xdisplacement.min())+str(xdisplacement.max())
+        print "The min-max y displacement is:"+str(ydisplacement.min())+str(ydisplacement.max())
+        print "The min-max z displacement is:"+str(zdisplacement.min())+str(zdisplacement.max())
+	return np.array([xdisplacement,ydisplacement,zdisplacement])
+
+####################trash can#####################
+# The old function used ofr generating mask, using interpolation
 def generatingMask(densityField,binNumber,gridNumber,randomParticle = np.array([]),gridMax = 0.0):
 	x = np.where(densityField!=0)[0]
 	index = np.where(np.bincount(x)>0)[0]
@@ -127,5 +195,21 @@ def generatingMask(densityField,binNumber,gridNumber,randomParticle = np.array([
 		result = randomParticle[survivor]
 		return result	
 
-#def generatingMaskedField(field,mask):
-	
+def computeDisplacement(densityField,wavenumber,boxsize,bias):
+	vectorAxis = np.array([[0.j,0.j,0.j] for i in range(densityField.shape[0])])
+	displacementField = [0,0,0]
+	dk = 2*np.pi/boxsize
+	Axis = np.arange(densityField.shape[0])
+	mesh = np.array(np.meshgrid(Axis,Axis,Axis))
+	xindex = mesh[0]
+	yindex = mesh[1]
+	zindex = mesh[2]
+	displacementField[0] = np.fft.ifftn(densityField*1j*dk*xindex/(bias*wavenumber))
+	displacementField[1] = np.fft.ifftn(densityField*1j*dk*yindex/(bias*wavenumber))
+	displacementField[2] = np.fft.ifftn(densityField*1j*dk*zindex/(bias*wavenumber))
+	displacementField = np.array(displacementField)
+	print 'The minmax disaplcement is'
+	print displacementField.min(),displacementField.max()
+	print 'The rms displacement is'
+	print np.sqrt(np.mean(displacementField*displacementField))
+	return np.array(displacementField)
